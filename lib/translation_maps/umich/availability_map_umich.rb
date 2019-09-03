a9a1a8bc267d35116e8e7e079d7eb9c3062af74a
Tim Prettyman <timothy@umich.edu>
2019-09-03 11:49:06 -0400

    Change "Available

--- lib/translation_maps/umich/availability_map_umich.rb
+++ lib/translation_maps/umich/availability_map_umich.rb
@@ -1,6 +1,6 @@
 {
     "avail_ht_fulltext"=>"Full text available online via HathiTrust",
-    "avail_circ"=>"Circulating volumes",
+    "avail_circ"=>"Circulating Items",
     "avail_checkout"=>"Available to check out",
     "avail_online"=>"Available online",
     "avail_recall"=>"Checked out (Available for recall)",
